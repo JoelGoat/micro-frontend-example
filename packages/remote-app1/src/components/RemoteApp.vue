010bd8b90435acdc30bf1eb687f14d2180c8fbce
Taekyeom Lee <tkyeom96@gmail.com>
2022-10-05 23:31:53 +0900

    feat: setup vue config in remote app1

---
+++ packages/remote-app1/src/components/RemoteApp.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>Remote App 1</div>
+  <div>Vue-Cli + Webpack</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
--- packages/remote-app1/vue.config.js
+++ packages/remote-app1/vue.config.js
@@ -1,4 +1,31 @@
 const { defineConfig } = require('@vue/cli-service')
+const webpack = require('webpack')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  pages: {
+    index: {
+      entry: './src/index.js',
+    },
+  },
+  publicPath: 'auto',
+  configureWebpack: {
+    optimization: {
+      splitChunks: false,
+    },
+    plugins: [
+      new webpack.container.ModuleFederationPlugin({
+        name: 'remote_app1',
+        filename: 'remoteEntry.js',
+        exposes: {
+          './RemoteApp.vue': './src/components/RemoteApp.vue',
+        },
+        shared: {
+          vue: {
+            singleton: true,
+          },
+        },
+      }),
+    ],
+  },
+  transpileDependencies: true,
 })
