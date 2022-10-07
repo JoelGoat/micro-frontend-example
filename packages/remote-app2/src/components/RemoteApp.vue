bb42a02ee6a91314f4facc863fb660afc6c18aa0
Taekyeom Lee <tkyeom96@gmail.com>
2022-10-07 22:51:33 +0900

    feat: setup vite config in remote app2

---
+++ packages/remote-app2/src/components/RemoteApp.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>Remote App</div>
+  <div>Vue3 + Vite</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
--- packages/remote-app2/vite.config.js
+++ packages/remote-app2/vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import federation from '@originjs/vite-plugin-federation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    federation({
+      name: 'remote_app2',
+      filename: 'remoteEntry.js',
+      remotes: {},
+      exposes: {
+        './RemoteApp': {
+          name: 'remote_app2',
+          import: './src/components/RemoteApp.vue',
+        },
+      },
+      shared: ['vue'],
+    }),
+  ],
+  build: {
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        format: 'esm',
+        entryFileNames: 'assets/[name].js',
+        minifyInternalExports: false,
+      },
+    },
+  },
 })
